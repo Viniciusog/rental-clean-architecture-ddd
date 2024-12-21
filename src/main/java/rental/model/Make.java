bdf15a3561897eb17e29b3ad0cd880d9c3b41dbc
Vinícius Guimarães <vinijipa23@gmail.com>
2024-12-20 21:56:35 -0300

    Add Validation for Make value and tests

--- src/main/java/rental/model/Make.java
+++ src/main/java/rental/model/Make.java
@@ -5,7 +5,7 @@ public class Make {
     private final String value;
 
     public Make(String value) {
-        this.value = value;
+        this.value = Validation.required(value, "Make value is required.");
     }
 
     public String value() {
--- src/test/java/ut/model/MakeTest.java
+++ src/test/java/ut/model/MakeTest.java
@@ -1,9 +1,11 @@
 package ut.model;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import rental.model.Make;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.hamcrest.Matchers.is;
 
 
@@ -14,5 +16,16 @@ public class MakeTest {
     void createSuccessfully(String value) {
         Make make = new Make(value);
         assertThat(make.value(), is(value));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", " "})
+    void throwsExceptionWhenValueIsInvalid(String value) {
+        IllegalArgumentException expection =
+                assertThrows(IllegalArgumentException.class,
+                () -> new Make(value));
+
+        assertThat(expection.getMessage(), is("Make value is required."));
     }
 }
