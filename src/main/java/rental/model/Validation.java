38d3237725e0056c1bc12dc86b4149abd10604be
Vinícius Guimarães <vinijipa23@gmail.com>
2024-12-20 21:56:18 -0300

    Add Validation class

---
+++ src/main/java/rental/model/Validation.java
@@ -0,0 +1,27 @@
+package rental.model;
+
+public class Validation {
+
+    private Validation()
+    {
+
+    }
+
+    public static <T> T required(T value, String message) {
+        checkNull(value, message);
+        checkBlankString(value, message);
+        return value;
+    }
+
+    private static void checkNull(Object value, String message) {
+        if (value == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    private static void checkBlankString(Object value, String message) {
+        if (value instanceof String str && str.isBlank()) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
