4176cfeccf85cef99741e99a2c8675cdf01cc0a3
Vinícius Guimarães <vinijipa23@gmail.com>
2024-12-25 18:11:14 -0300

    Add Customer and CustomerTest classes

---
+++ src/main/java/rental/model/customer/Customer.java
@@ -0,0 +1,139 @@
+package rental.model.customer;
+
+import rental.Validation;
+import rental.model.Email;
+import rental.model.exception.CustomerIsAlreadyActiveException;
+import rental.model.exception.CustomerIsAlreadyInactiveException;
+
+import java.util.Objects;
+
+public class Customer {
+
+    private CustomerId id;
+    private CustomerName name;
+    private Email email;
+    private Boolean active;
+
+    public Customer(Builder builder) {
+        this.id = builder.id;
+        this.name = validateName(builder.name);
+        this.email = builder.email;
+        this.active = Validation.required(builder.active, "active is required");
+    }
+
+    public CustomerId id() {
+        return this.id;
+    }
+
+    public CustomerName name() {
+        return this.name;
+    }
+
+    public Email email() {
+        return this.email;
+    }
+
+    public Boolean isActive() {
+        return Boolean.TRUE.equals(active);
+    }
+
+    public void deactivate() {
+        if (!active) {
+            throw new CustomerIsAlreadyInactiveException(id);
+        }
+        active = false;
+    }
+
+    public void activate() {
+        if (active) {
+            throw new CustomerIsAlreadyActiveException(id);
+        }
+        active = true;
+    }
+
+    public void created(CustomerId id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, active);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other != null
+                && getClass().equals(other.getClass())
+                && equalsCasted((Customer) other);
+    }
+
+    private boolean equalsCasted(Customer other) {
+        return Objects.equals(id, other.id)
+                && Objects.equals(name, other.name)
+                && Objects.equals(email, other.email)
+                && Objects.equals(active, other.active);
+    }
+
+    public void update(CustomerName name, Email email) {
+        this.name = validateName(name);
+        this.email = email;
+    }
+
+    public CustomerName validateName(CustomerName name) {
+        return Validation.required(name, "name is required");
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private CustomerId id;
+        private CustomerName name;
+        private Email email;
+        private Boolean active = true;
+
+        private Builder() {
+
+        }
+
+        public Builder id(CustomerId id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder id(Long id) {
+            this.id = id == null ? null : CustomerId.of(id);
+            return this;
+        }
+
+        public Builder name(CustomerName name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name == null ? null : CustomerName.of(name);
+            return this;
+        }
+
+        public Builder email(Email email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email == null ? null : Email.of(email);
+            return this;
+        }
+
+        public Builder active(Boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public Customer build() {
+            return new Customer(this);
+        }
+    }
+}
---
+++ src/test/java/rental/ut/model/customer/CustomerTest.java
@@ -0,0 +1,144 @@
+package rental.ut.model.customer;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import rental.fixture.CustomerFixture;
+import rental.model.Email;
+import rental.model.customer.Customer;
+import rental.model.customer.CustomerName;
+import rental.model.exception.CustomerIsAlreadyActiveException;
+import rental.model.exception.CustomerIsAlreadyInactiveException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static rental.fixture.CustomerFixture.*;
+
+public class CustomerTest {
+
+    @Test
+    void createSuccessfully() {
+        Customer customer = Customer.builder()
+                .name(CUSTOMER_NAME)
+                .email(EMAIL)
+                .build();
+
+        assertThat(customer.id(), is(nullValue()));
+        assertThat(customer.name(), is(CUSTOMER_NAME));
+        assertThat(customer.email(), is(EMAIL));
+        assertThat(customer.isActive(), is(true));
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    void createFullSuccessfully(boolean active) {
+        Customer customer  = Customer.builder()
+                .id(CUSTOMER_ID)
+                .name(CUSTOMER_NAME)
+                .email(EMAIL)
+                .active(active)
+                .build();
+
+        assertThat(customer.id(), is(CUSTOMER_ID));
+        assertThat(customer.name(), is(CUSTOMER_NAME));
+        assertThat(customer.email(), is(EMAIL));
+        assertThat(customer.isActive(), is(active));
+    }
+
+    @Test
+    void throwsExceptionWhenCreatingWithNullName() {
+        Customer.Builder builder = CustomerFixture.builder()
+                .name((String)null);
+
+        IllegalArgumentException exception  = assertThrows(IllegalArgumentException.class,
+                () -> builder.build());
+
+        assertThat(exception.getMessage(), is("name is required"));
+    }
+
+    @Test
+    void throwsExceptionWhenCreatingWithNullActive() {
+        Customer.Builder builder = CustomerFixture.builder()
+                .active(null);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> builder.build());
+
+        assertThat(exception.getMessage(), is("active is required"));
+    }
+
+    @Test
+    void doesNotThrowsExceptionWhenCreatingWithNullEmail() {
+        Customer customer = CustomerFixture.builder()
+                .email((String) null).build();
+
+        assertThat(customer.email(), is(nullValue()));
+    }
+
+    @Test
+    void mustDeactivate() {
+        Customer customer = anActiveCustomer().build();
+        assertThat(customer.isActive(), is(true));
+
+        customer.deactivate();
+
+        assertThat(customer.isActive(), is(false));
+    }
+
+    @Test
+    void throwsExceptionWhenDeactivatingAlreadyInactive() {
+        Customer customer = anInactiveCustomer().build();
+        assertThat(customer.isActive(), is(false));
+
+        assertThrows(CustomerIsAlreadyInactiveException.class, customer::deactivate);
+    }
+
+    @Test
+    void mustActivate() {
+        Customer customer = anInactiveCustomer().build();
+        assertThat(customer.isActive(), is(false));
+
+        customer.activate();
+
+        assertThat(customer.isActive(), is(true));
+    }
+
+    @Test
+    void throwsExceptionWhenActivatingAlreadyActive() {
+        Customer customer = anActiveCustomer().build();
+        assertThat(customer.isActive(), is(true));
+
+        assertThrows(CustomerIsAlreadyActiveException.class, customer::activate);
+    }
+
+    @Test
+    void mustUpdateId() {
+        Customer customer = aCustomerWithoutId().build();
+        assertThat(customer.id(), is(nullValue()));
+
+        customer.created(CUSTOMER_ID);
+
+        assertThat(customer.id(), is(CUSTOMER_ID));
+    }
+
+    @Test
+    void mustUpdate() {
+        Customer customer = aCustomerWithId().build();
+        assertThat(customer.name(), not("Updated"));
+        assertThat(customer.email(), not("updated@server.com"));
+
+        customer.update(CustomerName.of("Updated"),
+                Email.of("updated@server.com"));
+
+        assertThat(customer.name().value(), is("Updated"));
+        assertThat(customer.email().address(), is("updated@server.com"));
+    }
+
+    @Test
+    void equalsAndHashCode() {
+        EqualsVerifier.simple().forClass(Customer.class).usingGetClass().verify();
+    }
+}
