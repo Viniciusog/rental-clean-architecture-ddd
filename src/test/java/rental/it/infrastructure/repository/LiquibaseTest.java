dc0fbbfa33ad839815f86b91cdaa619e734cfbc2
Vinícius Guimarães <vinijipa23@gmail.com>
2024-12-27 16:43:52 -0300

    Add changelog.yml and application.properties for tests

--- src/test/java/rental/it/infrastructure/repository/LiquibaseTest.java
+++ src/test/java/rental/it/infrastructure/repository/LiquibaseTest.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 
 @JdbcTest
