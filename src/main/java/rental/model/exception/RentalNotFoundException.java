f7f601566cac345288b5bc26d72642c9d3357d31
Vinícius Guimarães <vinijipa23@gmail.com>
2024-12-31 23:56:37 -0300

    Add RentalNotFoundException

---
+++ src/main/java/rental/model/exception/RentalNotFoundException.java
@@ -0,0 +1,9 @@
+package rental.model.exception;
+
+import rental.model.rental.RentalId;
+
+public class RentalNotFoundException extends AbstractNotFoundException{
+    public RentalNotFoundException(RentalId rentalId) {
+        super("Rental not found with id: " + rentalId);
+    }
+}
