876e5ae739c1d975ae4c58474b71b491742613e3
Vinícius Guimarães <vinijipa23@gmail.com>
2025-01-02 19:05:35 -0300

    Update CreateRentalUseCase and tests to use CarAvailabilityChecker

--- src/main/java/rental/application/rental/CreateRentalUseCase.java
+++ src/main/java/rental/application/rental/CreateRentalUseCase.java
@@ -2,8 +2,11 @@ package rental.application.rental;
 
 import org.apache.commons.collections4.bidimap.AbstractBidiMapDecorator;
 import rental.application.AppTransaction;
+import rental.model.car.CarAvailabilityChecker;
 import rental.model.car.CarId;
+import rental.model.car.CarRepository;
 import rental.model.customer.CustomerId;
+import rental.model.exception.CarNotAvailableException;
 import rental.model.rental.Rental;
 import rental.model.rental.RentalId;
 import rental.model.rental.RentalRepository;
@@ -15,13 +18,21 @@ public class CreateRentalUseCase {
 
     private AppTransaction transaction;
     private RentalRepository repository;
+    private CarAvailabilityChecker carAvailabilityChecker;
 
-    public CreateRentalUseCase(AppTransaction transaction, RentalRepository repository) {
+    public CreateRentalUseCase(AppTransaction transaction, RentalRepository repository, CarAvailabilityChecker carAvailabilityChecker) {
         this.transaction = transaction;
         this.repository = repository;
+        this.carAvailabilityChecker = carAvailabilityChecker;
     }
 
-    public RentalId execute(CustomerId customerId, CarId carId, LocalDate initialDate, LocalDate endDate, BigDecimal totalPrice) {
+    public RentalId execute(CustomerId customerId, CarId carId,
+                            LocalDate initialDate, LocalDate endDate,
+                            BigDecimal totalPrice) {
+
+        if (!carAvailabilityChecker.isCarAvailable(carId, initialDate, endDate)) {
+            throw new CarNotAvailableException(carId);
+        }
         Rental rental = Rental.builder()
                 .carId(carId)
                 .customerId(customerId)
--- src/test/java/rental/ut/application/rental/CreateRentalUseCaseTest.java
+++ src/test/java/rental/ut/application/rental/CreateRentalUseCaseTest.java
@@ -7,37 +7,44 @@ import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import rental.application.AppTransaction;
 import rental.application.rental.CreateRentalUseCase;
 import rental.fixture.AppTransactionFixture;
+import rental.model.car.CarAvailabilityChecker;
 import rental.model.car.CarId;
 import rental.model.customer.Customer;
 import rental.model.customer.CustomerId;
+import rental.model.exception.CarNotAvailableException;
 import rental.model.rental.Rental;
 import rental.model.rental.RentalId;
 import rental.model.rental.RentalRepository;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static rental.fixture.CarFixture.CAR_ID;
 import static rental.fixture.CustomerFixture.CUSTOMER_ID;
 import static rental.fixture.RentalFixture.*;
-import static rental.ut.model.Rental.RentalTest.*;
-
+import static org.hamcrest.Matchers.is;
 
 public class CreateRentalUseCaseTest {
 
     private AppTransaction transaction;
     private RentalRepository repository;
     private CreateRentalUseCase useCase;
+    private CarAvailabilityChecker carAvailabilityChecker;
 
     @BeforeEach
     public void beforeEach() {
         transaction = AppTransactionFixture.mockedTransaction();
         repository = mock(RentalRepository.class);
-        useCase = new CreateRentalUseCase(transaction, repository);
+        carAvailabilityChecker = mock(CarAvailabilityChecker.class);
+        useCase = new CreateRentalUseCase(transaction, repository, carAvailabilityChecker);
         AppTransactionFixture.assertThatInTransaction(transaction).when(repository).save(any());
     }
 
     @Test
     public void mustCreateSuccessfully() {
+        when(carAvailabilityChecker.isCarAvailable(CAR_ID, RENTAL_INITIAL_DATE, RENTAL_END_DATE))
+                .thenReturn(true);
         Rental expectedRental = Rental.builder()
                 .customerId(CUSTOMER_ID)
                 .carId(CAR_ID)
@@ -54,4 +61,21 @@ public class CreateRentalUseCaseTest {
 
         verify(repository).save(expectedRental);
     }
+
+    @Test
+    public void throwsExceptionWhenCarIsNotAvailable() {
+        when(carAvailabilityChecker.isCarAvailable(CAR_ID, RENTAL_INITIAL_DATE, RENTAL_END_DATE))
+                .thenReturn(false);
+
+        CarNotAvailableException exception = assertThrows(CarNotAvailableException.class, () -> {
+           useCase.execute(
+                   CUSTOMER_ID,
+                   CAR_ID,
+                   RENTAL_INITIAL_DATE,
+                   RENTAL_END_DATE,
+                   RENTAL_TOTAL_PRICE);
+        });
+
+        assertThat(exception.getMessage(),
+                is("Car with id: " + CAR_ID.value() + " is not available to rent."));    }
 }
