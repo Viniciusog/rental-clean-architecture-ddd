9ff412ce404c90473caf1dd6b5cbcdd20f2c0dc0
Vinícius Guimarães <vinijipa23@gmail.com>
2025-01-03 18:07:30 -0300

    Update RentalPriceCalculator and tests to use carId

--- src/main/java/rental/model/rental/RentalPriceCalculator.java
+++ src/main/java/rental/model/rental/RentalPriceCalculator.java
@@ -1,5 +1,10 @@
 package rental.model.rental;
 
+import rental.model.car.Car;
+import rental.model.car.CarId;
+import rental.model.car.CarRepository;
+import rental.model.exception.CarNotFoundException;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
@@ -7,10 +12,19 @@ import java.time.temporal.ChronoUnit;
 
 public class RentalPriceCalculator {
 
-    public BigDecimal execute(BigDecimal dailyPrice,
-                                           LocalDate initialDate,
-                                           LocalDate endDate) {
-        long days = ChronoUnit.DAYS.between(initialDate, endDate) + 1;
+    private CarRepository carRepository;
+
+    public RentalPriceCalculator(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public BigDecimal execute(CarId carId, DateTimeRange timeRange) {
+
+        Car car = carRepository.getById(carId)
+                .orElseThrow(() -> new CarNotFoundException(carId));
+
+        BigDecimal dailyPrice = car.dailyPrice();
+        long days = ChronoUnit.DAYS.between(timeRange.start(), timeRange.end()) + 1;
         return dailyPrice.multiply(BigDecimal.valueOf(days)).setScale(2, RoundingMode.HALF_EVEN);
     }
 }
--- src/test/java/rental/ut/model/Rental/RentalPriceCalculatorTest.java
+++ src/test/java/rental/ut/model/Rental/RentalPriceCalculatorTest.java
@@ -2,47 +2,82 @@ package rental.ut.model.Rental;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+import rental.model.car.Car;
+import rental.model.car.CarRepository;
+import rental.model.exception.CarNotFoundException;
+import rental.model.rental.DateTimeRange;
 import rental.model.rental.RentalPriceCalculator;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import static rental.fixture.CarFixture.CAR_ID;
+import static rental.fixture.CarFixture.aCarWithId;
+import static rental.fixture.RentalFixture.RENTAL_TIME_RANGE;
 
 public class RentalPriceCalculatorTest {
 
     private RentalPriceCalculator calculator;
+    private CarRepository carRepository;
 
     @BeforeEach
     void beforeEach() {
-        calculator = new RentalPriceCalculator();
+        carRepository = mock(CarRepository.class);
+        calculator = new RentalPriceCalculator(carRepository);
     }
 
     @Test
-    void calculateRentalPriceSuccessfully() {
+    void calculateRentalPriceSuccessfullyForMultipleDays() {
         BigDecimal carDailyPrice = BigDecimal.valueOf(100.50);
-        BigDecimal expectedRentalPrice = BigDecimal.valueOf(2).multiply(carDailyPrice)
+        when(carRepository.getById(CAR_ID))
+                .thenReturn(Optional.of(aCarWithId().dailyPrice(carDailyPrice).build()));
+        BigDecimal expectedRentalPrice = BigDecimal.valueOf(3).multiply(carDailyPrice)
                 .setScale(2, RoundingMode.HALF_EVEN);
-        LocalDate initialDate = LocalDate.of(2025, 01, 01);
-        LocalDate endDate = LocalDate.of(2025, 01, 02);
+        Instant startTime = LocalDateTime.of(2025, 1, 1, 6, 0, 0).toInstant(ZoneOffset.UTC);
+        Instant endTime = LocalDateTime.of(2025, 1, 3, 6, 0, 0).toInstant(ZoneOffset.UTC);
 
-        BigDecimal result = calculator.execute(carDailyPrice, initialDate, endDate);
+        BigDecimal result = calculator.execute(CAR_ID, DateTimeRange.of(startTime, endTime));
 
+        verify(carRepository).getById(CAR_ID);
         assertThat(result, is(expectedRentalPrice));
     }
 
     @Test
-    void calculateSameDayRentalPriceSuccessfully() {
+    void calculateRentalPriceSuccessfullyForOneDay() {
         BigDecimal carDailyPrice = BigDecimal.valueOf(100.50);
+        when(carRepository.getById(CAR_ID))
+                .thenReturn(Optional.of(aCarWithId().dailyPrice(carDailyPrice).build()));
         BigDecimal expectedRentalPrice = BigDecimal.valueOf(1).multiply(carDailyPrice)
                 .setScale(2, RoundingMode.HALF_EVEN);
-        LocalDate initialDate = LocalDate.of(2025, 01, 01);
-        LocalDate endDate = LocalDate.of(2025, 01, 01);
+        Instant startTime = LocalDateTime.of(2025, 1, 1, 6, 0, 0).toInstant(ZoneOffset.UTC);
+        Instant endTime = LocalDateTime.of(2025, 1, 1, 23, 0, 0).toInstant(ZoneOffset.UTC);
 
-        BigDecimal result = calculator.execute(carDailyPrice, initialDate, endDate);
+        BigDecimal result = calculator.execute(CAR_ID, DateTimeRange.of(startTime, endTime));
 
+        verify(carRepository).getById(CAR_ID);
         assertThat(result, is(expectedRentalPrice));
+    }
+
+    @Test
+    void throwsExceptionWhenCarWasNotFound() {
+        when(carRepository.getById(CAR_ID)).thenReturn(Optional.empty());
+
+        CarNotFoundException exception = assertThrows(CarNotFoundException.class, () -> {
+            calculator.execute(CAR_ID, RENTAL_TIME_RANGE);
+        });
+
+        verify(carRepository).getById(CAR_ID);
+        assertThat(exception.getMessage(), is("Car not found with id: " + CAR_ID.value()));
     }
 }
