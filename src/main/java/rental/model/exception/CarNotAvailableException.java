87627e2a76a4602e0e853659e2e44e88e68955cd
Vinícius Guimarães <vinijipa23@gmail.com>
2025-01-05 23:10:55 -0300

    Update CarNotAvailableException to include DateTimeRange in constructor

--- src/main/java/rental/model/exception/CarNotAvailableException.java
+++ src/main/java/rental/model/exception/CarNotAvailableException.java
@@ -1,9 +1,11 @@
 package rental.model.exception;
 
 import rental.model.car.CarId;
+import rental.model.rental.DateTimeRange;
 
 public class CarNotAvailableException extends AbstractConflictException {
-    public CarNotAvailableException(CarId carId) {
-        super("Car with id: " + carId.value() + " is not available to rent.");
+    public CarNotAvailableException(CarId carId, DateTimeRange timeRange) {
+        super(String.format("Car with id: %s is not available for the given time range: %s to %s",
+                carId.value(), timeRange.start(), timeRange.end()));
     }
 }
