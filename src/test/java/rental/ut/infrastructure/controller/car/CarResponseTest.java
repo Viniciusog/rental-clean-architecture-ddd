64f01d20fd72871bd2d36f011601cdf3c95c4fc9
Vinícius Guimarães <vinijipa23@gmail.com>
2025-01-07 23:58:19 -0300

    Add CarResponseTest

---
+++ src/test/java/rental/ut/infrastructure/controller/car/CarResponseTest.java
@@ -0,0 +1,25 @@
+package rental.ut.infrastructure.controller.car;
+
+import org.junit.jupiter.api.Test;
+import rental.infrastructure.controller.car.CarResponse;
+import rental.model.car.Car;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static rental.fixture.CarFixture.aCarWithId;
+import static org.hamcrest.Matchers.is;
+
+public class CarResponseTest {
+
+    @Test
+    void mustCreateSuccessfullyFromCar() {
+        Car car = aCarWithId().build();
+
+        CarResponse response = CarResponse.of(car);
+
+        assertThat(response.id(), is(car.id().value()));
+        assertThat(response.dailyPrice(), is(car.dailyPrice()));
+        assertThat(response.make(), is(car.model().make().value()));
+        assertThat(response.model(), is(car.model().name()));
+        assertThat(response.year(), is((short)car.year().getValue()));
+    }
+}
